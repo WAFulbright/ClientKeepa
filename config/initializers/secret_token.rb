a817c110445469cc7e32c45906f761d071ec3a76
2100-Solutions <bill@2100solutions.com>
2016-07-06 20:25:07 -0700

    Initial Commit for KeepaClient

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KeepaClient::Application.config.secret_token = '6e62a6d26b32c454eeed719de6e5e067aa519284bd006b7ba7945ae371e755612e66a00420afbb61c5ddf0a7ad7ebc69c4e028bd996c68eb10fa249475d06433'
